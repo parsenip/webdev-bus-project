674de11ec37d379027b6f4c54511c0283596e75a
Amriganati <30274109+Amriganati@users.noreply.github.com>
2018-04-18 13:58:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+<DOCTYPE js>
+  <script>
+    function initMap() {
+  
+  var map = new google.maps.Map(document.getElementById('map')
+  
+}
+</script>
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCK0qkhAiByKvp2975fyjXw5tEmN4PG1io&callback=initMap"
+</script>
